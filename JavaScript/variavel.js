084d878eb5e3d8a7f4180f65aa27ad791d64e121
Nicolas191028 <du.antunes@icloud.com>
2025-09-01 15:51:26 -0300

    Aprendnedo variaveis

---
+++ JavaScript/variavel.js
@@ -0,0 +1,10 @@
+let mensagem = "Olá mundo";
+
+const nomeCompleto = "Nicolas Eduardo Antunes Da Silva Lima";
+
+let idade = 16;
+
+// Resultado
+console.log(mensagem);
+console.log(nomeCompleto);
+console.log("Meu nome é: " + nomeCompleto + " e minha idade é: " + idade + " Anos");
