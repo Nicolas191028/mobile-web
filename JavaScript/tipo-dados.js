61352883ca4849234e93039138a54ef7758f2adb
Nicolas191028 <du.antunes@icloud.com>
2025-09-01 16:03:53 -0300

    Descobrindo diferentes tipos de variaveis

---
+++ JavaScript/tipo-dados.js
@@ -0,0 +1,19 @@
+// String 
+let nome = "Nicolas";
+
+// Inteiro
+let idade = 16;
+
+// Booleano
+let casado = false;
+
+// Números flutuantes com casas decimais
+let valorProduto = 5.35;
+
+// Resultados 
+console.log("Meu nome é " + nome);
+console.log("Minha idade é: ", idade);
+console.log("Casado? ", casado);
+console.log("R$", valorProduto);
+console.log(typeof idade);
+console.log(typeof nome);
