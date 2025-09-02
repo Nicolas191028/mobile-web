5aedc8ba707dc937419fb75a047caac101dffeb0
Nicolas191028 <du.antunes@icloud.com>
2025-09-02 16:16:39 -0300

    Adicionando functions e buttons nas páginas calc.js e calc.html

--- JavaScript/calc.js
+++ JavaScript/calc.js
@@ -1,15 +1,27 @@
 // Duas variáveis
-let num1 = Number(prompt("Digite um número"));
-let num2 = Number(prompt("Digite outro número"));
-
+let num1 = 10;
+let num2 = 5;
 // Operações
 let soma = num1 + num2;
-let sub = num1 - num2;
-let mult = num1 * num2;
-let div = num1 / num2;
 
-// Imprimir as respostas
-console.log("Soma:", soma);
-console.log("Subtração:", sub);
-console.log("Multiplicação:", mult);
-console.log("Divisão:", div);
+function somar() {
+    console.log(soma);
+}
+
+ let subt = num1 - num2;
+
+ function sub() {
+    console.log(subt);
+ }
+
+let divi = num1 / num2;
+
+function div() {
+    console.log(divi);
+}
+
+let multi = num1 * num2;
+
+function mult() {
+    console.log(multi);
+}
