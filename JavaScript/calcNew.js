619572c0c482496fd6628f5979d3b9b61358d6c3
Nicolas191028 <du.antunes@icloud.com>
2025-09-03 16:14:27 -0300

    Fazendo calculos na página calcNew.js

---
+++ JavaScript/calcNew.js
@@ -0,0 +1,37 @@
+// Funções
+function somar(text, num1, num2) {
+    console.log(text, num1 + num2);
+}
+
+function subtrair(text, num1, num2) {
+    console.log(text, num1 - num2);
+}
+
+function multiplicar(text, num1, num2) {
+    console.log(text, num1 * num2);
+}
+
+function dividir(text, num1, num2) {
+    console.log(text, num1 / num2);
+}
+
+// Imprimir funções
+somar("10 + 10 = ", 10, 10); // 10 + 10
+somar("10 + 20 = ", 10, 20); // 10 + 20
+somar("10 + 30 = ", 10, 30); // 10 + 30
+somar("3 + 3 = ", 3, 3); // 3 + 3
+
+subtrair("10 - 10 = ", 10, 10); // 10 - 10
+subtrair("10 - 20 = ", 10, 20); // 10 - 20
+subtrair("10 - 30 = ", 10, 30); // 10 - 30
+subtrair("3 - 3 = ", 3, 3); // 3 - 3
+
+multiplicar("10 x 10 = ", 10, 10); // 10 * 10
+multiplicar("10 x 20 = ", 10, 20); // 10 * 20
+multiplicar("10 x 30 = ", 10, 30); // 10 * 30
+multiplicar("3 x 3 = ", 3, 3); // 3 * 3
+
+dividir("10 / 10 = ", 10, 10); // 10 / 10
+dividir("10 / 20 = ", 10, 20); // 10 / 20
+dividir("10 / 30 = ", 10, 30); // 10 / 30
+dividir("3 / 3 = ", 3, 3); // 3 / 3
