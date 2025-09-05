5ab9bb343e7e398975089e66293cfaa2e79422e9
Nicolas191028 <du.antunes@icloud.com>
2025-09-05 14:50:16 -0300

    Adicionando else e if

---
+++ JavaScript/js-exemplos/idade.js
@@ -0,0 +1,7 @@
+let idade = 16;
+
+if (idade >= 18) {
+    console.log("Pessoa pode ir para festa");
+} else {
+    console.log("Pessoa não pode ir para a festa");
+}
---
+++ JavaScript/js-exemplos/nota.js
@@ -0,0 +1,7 @@
+let nota = 5;
+
+if (nota >= 6) {
+    console.log("Você aluno está aprovado");
+} else {
+    console.log("Você aluno está reprovado");
+}
