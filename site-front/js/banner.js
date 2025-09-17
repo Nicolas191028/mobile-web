8503796d5cf1ce4e1a8c701ff8b37a25b6ac4eff
Nicolas191028 <du.antunes@icloud.com>
2025-09-17 16:23:52 -0300

    Adicionando modificações no banner

--- site-front/js/banner.js
+++ site-front/js/banner.js
@@ -1,1 +1,19 @@
-console.log("Banner");
+const imagens = ["../../img/img-1.jpg", "../../img/img-2.jpg", "../../img/img-3.jpg", "../../img/img-4.jpg", "../../img/img-5.jpg", "../../img/img-6.jpg", "../../img/img-7.jpg", "../../img/img-8.jpg", "../../img/img-9.jpg", "../../img/img-0.jpg"];
+//                         0                      1                  2                      3                          4                  5                      6
+
+let index = 0; // começa na primeira imagem
+const banner = document.getElementById("banner");
+
+function trocarImagem() {
+    index++; // Vai para próxima imagem
+
+    if (index >= imagens.length) {
+        index = 0; // Se passou do ultimo, volta para o primeiro
+    }
+
+    banner.src = imagens[index]
+}
+
+// chamar a função
+// trocarImagem();
+setInterval(trocarImagem, 3000 );
