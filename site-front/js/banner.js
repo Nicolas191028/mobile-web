15ee71ff57a6980ec262db4d873badf7b31c8af2
Nicolas191028 <du.antunes@icloud.com>
2025-09-18 16:04:19 -0300

    Alterações no banner

--- site-front/js/banner.js
+++ site-front/js/banner.js
@@ -1,5 +1,5 @@
 const imagens = ["../../img/img-1.jpg", "../../img/img-2.jpg", "../../img/img-3.jpg", "../../img/img-4.jpg", "../../img/img-5.jpg", "../../img/img-6.jpg", "../../img/img-7.jpg", "../../img/img-8.jpg", "../../img/img-9.jpg", "../../img/img-0.jpg"];
-//                         0                      1                  2                      3                          4                  5                      6
+//                         0                      1                  2                      3                          4                  5                      6                      7                      8                          9
 
 let index = 0; // começa na primeira imagem
 const banner = document.getElementById("banner");
