5a6deabbeb91041b4274b81b0d8a30a6f265f422
Nicolas191028 <du.antunes@icloud.com>
2025-09-18 16:20:20 -0300

    Adicionando codigos diferentes

--- projeto-b2/src/App.jsx
+++ projeto-b2/src/App.jsx
@@ -3,9 +3,36 @@ function App() {
 
   return (
   <>
-    <h1>Olá Rodigão</h1>
-    <h2>Giovanni</h2>
-  </>
+    <form>
+      <h1>Olá Rodigão</h1>
+      <h2>Giovanni</h2>
+      <h3>Nicolas</h3>
+      <h4>Brunão 1</h4>
+      <h5>Brunão 2</h5>
+      <h6>Stefanny</h6>
+    </form>
+    <section>
+      <span>Leandro</span>
+      <small>Dudu</small>
+      <strong>Caique</strong>
+    </section>
+    <div>
+      <hr />
+      <br />
+      <ul>
+        <li>Mayra</li>
+      </ul>
+      <ol>
+        <li>Giovanna</li>
+      </ol>
+      <a href="#"></a>
+    </div>  
+    <main>
+      <marquee>Doris</marquee>
+      {/* Jorge */}
+      <p>Dri</p>
+    </main>  
+    </>
   )
 }
 
