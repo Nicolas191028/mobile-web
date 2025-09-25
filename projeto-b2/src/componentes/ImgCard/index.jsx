a12cb32e992cf93052d911ba027feb6032c2a61d
Nicolas191028 <du.antunes@icloud.com>
2025-09-25 16:12:33 -0300

    Adicionando botão

--- projeto-b2/src/componentes/ImgCard/index.jsx
+++ projeto-b2/src/componentes/ImgCard/index.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import img1 from "../../assets/img-card1.jpg";
 import "./imgCard.css"
 
-export default function ImgCard() {
+export default function ImgCard(props) {
+    console.log(props)
   return (
-    <div className="image-card">olá Img - Popeto</div>
+    <div className="image-card">
+        <img src={img1} alt={props.caption || 'Imagem Padrão'} />
+        <p className="image-card-caption">
+            {props.caption}
+        </p>
+    </div>
   )
 }
