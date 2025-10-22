3f4518cf1aceb034d9bbe65d439f2ebdaecd685b
Nicolas191028 <du.antunes@icloud.com>
2025-10-22 16:14:45 -0300

    Estilizando cadastro

--- meuapp/src/Telas/Cadastro.js
+++ meuapp/src/Telas/Cadastro.js
@@ -20,6 +20,16 @@ export default function Cadastro() {
         const novoContato = {nome, telefone, imagem};
 
         axios.post(`http://10.0.2.2:3000/contatos`, novoContato)
+            .then((resposta) => {
+                if (resposta.status === 201) {
+                    setNome('');
+                    setTelefone('');
+                    setImagem('');
+                    navigation.navigate("ListaContatos")
+                } else {
+                    Alert.alert("Erro, falha ao adicionar contato")
+                }
+            })
     }
     
 
@@ -31,30 +41,34 @@ export default function Cadastro() {
                   textButton="Ir para Home"
                   onPress={ () => navigation.navigate("Home") }
         />
-        <Text style={estilos.label}>Nome:</Text>
-        <TextInput 
-            style={estilos.input}
-            value={nome}
-            onChange={setNome}
-            placeholder='Digite o Nome'
-        />
 
-        <Text style={estilos.label}>Telefone:</Text>
-        <TextInput 
-            style={estilos.input}
-            value={telefone}
-            onChange={setTelefone}
-            placeholder='Digite o telefone'
-        />
-        <Text style={estilos.label}>Imgaem:</Text>
-        <TextInput 
-            style={estilos.input}
-            value={imagem}
-            onChange={setImagem}
-            placeholder='Coloque uma imagem'
-        />
+        <View style={estilos.containerInt}>
+            <Text style={estilos.cadastro}>Cadastro</Text>            
+            <Text style={estilos.label}>Nome:</Text>
+            <TextInput 
+                style={estilos.input}
+                value={nome}
+                onChangeText={setNome}
+                placeholder='Digite o Nome'
+            />
 
-        <Button title="Cadastrar" onPress={enviarContato}/>
+            <Text style={estilos.label}>Telefone:</Text>
+            <TextInput 
+                style={estilos.input}
+                value={telefone}
+                onChangeText={setTelefone}
+                placeholder='Digite o telefone'
+            />
+            <Text style={estilos.label}>Imgaem:</Text>
+            <TextInput 
+                style={estilos.input}
+                value={imagem}
+                onChangeText={setImagem}
+                placeholder='Coloque uma imagem'
+            />
+
+            <Button title="Cadastrar" onPress={enviarContato}/>
+        </View>
     </ScrollView>
   )
 }
@@ -69,9 +83,28 @@ const estilos = StyleSheet.create({
         padding: 20,
         margin: 20,
     },
+    cadastro: {
+        backgroundColor: '#fff',
+        borderRadius: 1.41,
+        padding: 20,
+        margin: 20, 
+        textAlign: "center",
+        fontSize: 40,
+        color: "red"    
+    },
+    containerInt: {
+        backgroundColor: '#fff',
+        borderRadius: 1.41,
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+        elevation: 2,
+        padding: 20,
+        margin: 20,
+    },
     label: {
         fontSize: 18,
-        marginBottom: 5
+        marginBottom: 5,
+        color: "orange"
     },
     input: {
         borderWidth: 1,
